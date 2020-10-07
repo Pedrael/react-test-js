ade821275badacfc2b1884e3150fd3fa22b6547a
ReidenXerx <34869259+ReidenXerx@users.noreply.github.com>
2020-10-07 17:52:02 +0300

    update

--- src/App.js
+++ src/App.js
@@ -4,22 +4,24 @@ import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
 import Loader from "./Loader";
-import Modal from './Modal/Modal';
+import Modal from "./Modal/Modal";
 
 function App() {
   const [todos, setTodos] = useState([]);
   //useState возвращает массив из 2 элементов - 1 элемент непосредственно то, что мы записываем в состояние, а 2 - функция, через которую мы сможем реактивно изменять состояние
-  const [loading, setLoading] = useState(true);
+  //const [loading, setLoading] = useState(true);
+  const loading = todos.length == 0
 
   useEffect(() => {
+    //триггерится при каждом рендере, если изменяется список зависимостей (стейты), если передаю пустой список, то триггерится только при монтировании, если не передаю ничего, то триггерится постоянно
     fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
       .then((response) => response.json())
       .then((todosServer) => {
         setTimeout(() => {
-          setTodos(todosServer)
-          setLoading(false)
+          setTodos(todosServer);
+          //setLoading(false);
         }, 2000);
-      });
+      })
   }, []); //что за список зависимостей
 
   function toggleTodo(id) {
@@ -55,16 +57,14 @@ function App() {
   return (
     <Context.Provider value={{ removeTodoMethod: removeTodo }}>
       <div className="wrapper">
-        <h1>React tutorial</h1>
-        <Modal/>
-        <AddTodo onCreateMethod={addTodoMethod} />
-        {loading && <Loader />}
+        <h1> React tutorial </h1> <Modal />
+        <AddTodo onCreateMethod={addTodoMethod} /> {loading && <Loader />}{" "}
         {todos.length ? (
           <TodoList todos={todos} onToggleMethod={toggleTodo} />
         ) : loading ? null : (
-          <p>No todos!</p>
-        )}
-      </div>
+          <p> No todos! </p>
+        )}{" "}
+      </div>{" "}
     </Context.Provider>
   );
 }
--- src/Todo/AddTodo.js
+++ src/Todo/AddTodo.js
@@ -1,34 +1,61 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-function useInputValueHook(defaultValue = '') {
-  const [value, setValue] = useState(defaultValue)
+//удобно для случаев, когда в форме множество полей для группировки работы с многими состояниями
+function useInputValueHook(defaultValue = "") {
+  //2 способа - изменение стейта, изменение пропсов, или запуск ререндера
 
+  const [value, setValue] = useState(defaultValue);
+  const [isFocused, setIsFocused] = useState(false);
+  //у инпута был лейбл введите больше 3 символов
+
+  const error = value.length < 3; //можно выносить условия в отдельные переменные
   return {
     bind: {
-      value,//ключ совпадает со значением, поэтому можно писать только значение
-      onChange: event => setValue(event.target.value)
+      value, //ключ совпадает со значением, поэтому можно писать только значение
+      error,
+      isFocused,
+      onChange: (event) => setValue(event.target.value),
+      onFocus: () => setIsFocused(true), // focus
+      onBlur: () => setIsFocused(false), // unfocus
     },
-    clear: () => setValue(''),
-    value: () => value
-  }
+    clear: () => setValue(""),
+    value: () => value,
+  };
 }
 
-function AddTodo({onCreateMethod}) {
-  const inputHook = useInputValueHook('')
+//при фокусе происходит проверка на ошибку, если ошибка, то красный и появляется лейбл
+const GenericInput = (
+  { error, isFocused, ...restProps } //нельзя распаковывать элементы, не являщиеся родными в хтмл элементах, если передаются такие элементы внутрь компонента, то их нужно отделять от распаковываемых спредом и передавать отдельно (для стрикт мода)
+) => (
+  <>
+    <input
+      id="input"
+      {...restProps}
+      style={error && isFocused ? { background: "red" } : {}}
+    />
+    {console.log(error)}
+    {error && isFocused && <label htmlFor="input">some error message</label>}
+  </>
+);
+
+function AddTodo({ onCreateMethod }) {
+  const inputHook = useInputValueHook("");
 
   function submitHandler(event) {
     event.preventDefault();
     if (inputHook.value().trim()) {
       onCreateMethod(inputHook.value());
-      inputHook.clear()
+      inputHook.clear();
     }
   }
-  
+
+  //спред распаковывает массивы по порядку (1 2 3 4), а объекты по ключам (каждое поле в соответствующую переменную, которая называется как ключ)
+
   return (
     <form style={{ marginBottom: "1rem" }} onSubmit={submitHandler}>
-      <input {...inputHook.bind} />
-      <button type="submit">Add todo</button>
+      <GenericInput {...inputHook.bind} />
+      <button type="submit"> Add todo </button>{" "}
     </form>
   );
 }
