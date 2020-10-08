944317b9852de609fc605f57e2926fa2925a7a02
ReidenXerx <vadik44444444@yandex.ua>
2020-10-08 15:10:04 +0300

    commit

--- src/Todo/TodoItem.js
+++ src/Todo/TodoItem.js
@@ -1,7 +1,8 @@
-import React, { useContext } from "react"
+import React, { Component, Fragment, useContext } from "react"
 import PropTypes from 'prop-types'
 import Context from '../context';
 
+/*
 const styles = {
   li: {
     display: 'flex',
@@ -30,7 +31,7 @@ function TodoItem({ todo, index, onChangeMethod }) { //передаем мето
   return (
     <li style={styles.li}>
       <span className={classes.join(' ')}>
-        {/*jsx comment*/}
+        {}
         <input
           checked={todo.completed}
           type="checkbox"
@@ -54,3 +55,58 @@ TodoItem.propTypes = {
 }
 
 export default TodoItem;
+*/
+
+export default class Todo extends Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor');
+  }
+  fixhook () {
+    const {removeTodoMethod} = useContext(Context)
+    return removeTodoMethod
+  }
+  render() {
+    const classes = []
+  
+    if (this.props.todo.completed) {
+      classes.push('done')
+    }
+    //checked={todo.completed} если начальные значения комплитед не все фоллс, чтоб отображалось с самого начала как выполненное
+    const onClickHandler = () => this.fixhook()(this.props.todo.id)
+    const styles = {
+      li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+      },
+      input: {
+        marginRight: '1rem'
+      }
+    }
+    return (
+      <Fragment>
+        <li style={styles.li}>
+          <span className={classes.join(' ')}>
+            {}
+            <input
+              checked={this.props.todo.completed}
+              type="checkbox"
+              onChange={() => this.props.onChangeMethod(this.props.todo.id)}
+            />
+            <strong>{this.props.index + 1}</strong>
+            &nbsp;
+            {this.props.todo.title}
+          </span>
+
+
+          <button className='rm' onClick={onClickHandler}>&times;</button>
+        </li>
+      </Fragment>
+    )
+  }
+}
--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 import PropTypes from "prop-types";
 import TodoItem from "./TodoItem";
 import Context from "../context";
-
+/*
 const styles = {
   ul: {
     listStyle: "none",
@@ -36,6 +36,47 @@ function TodoList(props) {
 TodoList.propTypes = {
   todos: PropTypes.arrayOf(PropTypes.object).isRequired,
   onToggleMethod: PropTypes.func.isRequired,
-};
+};*/
 
-export default TodoList;
+//export default TodoList;
+
+export default class TodoListClass extends Component {
+  state = {
+    flag: false
+  }
+  constructor(props) {
+    super(props)
+    this.setState({flag: true})
+  }
+  render() {
+    const styles = {
+      ul: {
+        listStyle: "none",
+        margin: 0,
+        padding: 0,
+      },
+    };
+    return (
+      <Fragment>
+        <ul style={styles.ul}>
+          {this.props.todos.map((todo, index) => {
+            return (
+              <TodoItem
+                todo={todo}
+                key={todo.id}
+                index={index}
+                onChangeMethod={this.props.onToggleMethod}
+              />
+            );
+          })}
+          <Context.Consumer>
+            {(value) => {
+              console.log(value);
+            }}
+          </Context.Consumer>
+        </ul>
+      </Fragment>
+    )
+  }
+
+}
